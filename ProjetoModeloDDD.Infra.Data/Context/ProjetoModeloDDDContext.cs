034a48c67d973923d966d4713cb6bf0210a2d713
Samuel Souza <augusto_souza@edu.unifil.br>
2022-02-28 20:38:45 -0300

    Criado context e adicionado efcore.

---
+++ ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloDDDContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoModeloDDD.Domain.Entities;
+
+namespace ProjetoModeloDDD.Infra.Data.Context
+{
+    public class ProjetoModeloDDDContext : DbContext
+    {
+        public ProjetoModeloDDDContext(DbContextOptions<ProjetoModeloDDDContext> options) 
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        DbSet<Cliente> Cliente { get; set; }
+    }
+}
