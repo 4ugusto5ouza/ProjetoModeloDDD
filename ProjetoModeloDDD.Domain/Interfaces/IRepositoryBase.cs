a21cef169b0f265ac9ccd21b33c3bf204c8fe4bb
Samuel Souza <augusto_souza@edu.unifil.br>
2022-03-01 09:20:05 -0300

    Criado irepositorybase de tentity.

---
+++ ProjetoModeloDDD.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ProjetoModeloDDD.Domain.Interfaces
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        TEntity GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        void Save(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(TEntity obj);
+        void Dispose();
+    }
+}
