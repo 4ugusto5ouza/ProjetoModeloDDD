28747b8e926a325d10b8b9fc693cbd71efdf1b7b
Samuel Souza <augusto_souza@edu.unifil.br>
2022-03-05 20:06:58 -0300

    Criado contratos de serviços para o domínio.

---
+++ ProjetoModeloDDD.Domain/Interfaces/Services/IServiceBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ProjetoModeloDDD.Domain.Interfaces.Services
+{
+    public interface IServiceBase<TEntity> where TEntity : class
+    {
+        TEntity GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> GetAllAsNoTracking();
+        void Save(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(TEntity obj);
+        void Dispose();
+    }
+}
